aa27e9513e970507d401455ee5eb9d8eb3959285
angelo burias <angeloburias8@gmail.com>
2021-03-17 16:25:02 +0800

    add new tabs in resume

--- js/main.js
+++ js/main.js
@@ -21,6 +21,10 @@
   // Get the element with id="defaultOpen" and click on it
   document.getElementById("defaultOpen").click();
 
+
+
+
+  
 $(document).ready(function($) {
 
 	"use strict";
@@ -222,3 +226,19 @@ $(document).ready(function($) {
 
 });
 
+function openResumeTabs(evt, resumeSkills) {
+	var i, tabcontent, tablinks;
+	tabcontent = document.getElementsByClassName("resume-tabcontent");
+	for (i = 0; i < tabcontent.length; i++) {
+	  tabcontent[i].style.display = "none";
+	}
+	tablinks = document.getElementsByClassName("tablinks");
+	for (i = 0; i < tablinks.length; i++) {
+	  tablinks[i].className = tablinks[i].className.replace(" active", "");
+	}
+	document.getElementById(resumeSkills).style.display = "block";
+	evt.currentTarget.className += " active";
+  }
+
+  // Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpenResume").click();
