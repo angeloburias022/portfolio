db2bb540a14ac75cce7b550b26fa6ba00005a8cf
angelo burias <angeloburias8@gmail.com>
2021-03-17 23:24:39 +0800

    Add contact

--- js/main.js
+++ js/main.js
@@ -242,3 +242,20 @@ function openResumeTabs(evt, resumeSkills) {
 
   // Get the element with id="defaultOpen" and click on it
 document.getElementById("defaultOpenResume").click();
+
+
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
